109d9df619e2cd61de706d7134298cf37587ba56
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-11-26 21:26:29 +0900

    終了

--- LMS/CS中級/cs_m_lesson5.php
+++ LMS/CS中級/cs_m_lesson5.php
@@ -261,3 +261,229 @@ while($iterator != null){
 $numList->printList();
 ?>
 
+<br>
+
+<?php
+class node6
+{
+    public $data;
+    public $next;
+
+    public function __construct($data){
+        $this->data = $data;
+    }
+
+    public function addNextNode($newNode){
+        $tempNode = $this->next;
+        $this->next = $newNode;
+        $newNode->next = $tempNode;
+    }
+}
+class SinglyLinkedList6{
+    public $arr;
+    public $head;
+
+    public function __construct($arr){
+        $this->head = count($arr) > 0 ? new node6($arr[0]) : new node6(null);
+        $currentNode = $this->head;
+        for($i=1; $i<count($arr); $i++){
+            $currentNode->next = new node6($arr[$i]);
+            $currentNode = $currentNode->next;
+        }
+    }
+
+    public function at($index){
+        $iterator = $this->head;
+        for($i=0; $i<$index; $i++){
+            $iterator = $iterator->next;
+            if($iterator == null) return null;
+        }
+        return $iterator;
+    }
+
+    public function preAppend($newNode){
+        $newNode->next = $this->head;
+        $this->head = $newNode;
+    }
+
+    public function printList(){
+        $iterator = $this->head;
+        $str = "";
+        while($iterator != null){
+            $str .= $iterator->data. " ";
+            $iterator = $iterator->next;
+        }
+        echo $str .PHP_EOL;
+    }
+}
+$numList = new SinglyLinkedList6([35,23,546,67,86,234,56,767,34,1,98,78,555]);
+
+$numList->printList();
+$numList->preAppend(new node6(45));
+$numList->preAppend(new node6(236));;
+$numList->printLIst();
+?>
+
+
+<br>
+
+<?php
+class node7
+{
+    public $data;
+    public $next;
+
+    public function __construct($data){
+        $this->data = $data;
+    }
+
+    public function addNextNode($newNode){
+        $tempNode = $this->next;
+        $this->next = $newNode;
+        $newNode->next = $tempNode;
+    }
+}
+class SinglyLinkedList7{
+    public $arr;
+    public $head;
+
+    public function __construct($arr){
+        $this->head = count($arr) > 0 ? new node7($arr[0]) : new node7(null);
+        $currentNode = $this->head;
+        for($i=1; $i<count($arr); $i++){
+            $currentNode->next = new node7($arr[$i]);
+            $currentNode = $currentNode->next;
+        }
+    }
+
+    public function at($index){
+        $iterator = $this->head;
+        for($i=0; $i<$index; $i++){
+            $iterator = $iterator->next;
+            if($iterator == null) return null;
+        }
+        return $iterator;
+    }
+
+    public function preAppend($newNode){
+        $newNode->next = $this->head;
+        $this->head = $newNode;
+    }
+
+    public function afterAppend($data){
+        $newNode = new Node7($data);
+
+        if($this->head === null){
+            $this->head = $newNode;
+            return;
+        }
+
+        $current = $this->head;
+        while($current->next !== null){
+            $current = $current->next;
+        }
+        $current->next = $newNode;
+    }
+
+    public function printList(){
+        $iterator = $this->head;
+        $str = "";
+        while($iterator != null){
+            $str .= $iterator->data. " ";
+            $iterator = $iterator->next;
+        }
+        echo $str .PHP_EOL;
+    }
+}
+$numList = new SinglyLinkedList7([35,23,546,67,86,234,56,767,34,1,98,78,555]);
+
+$numList->printList();
+$numList->afterAppend(45);
+$numList->afterAppend(236);;
+$numList->printLIst();
+?>
+
+
+<br>
+
+<?php
+class node8
+{
+    public $data;
+    public $next;
+
+    public function __construct($data){
+        $this->data = $data;
+    }
+
+    public function addNextNode($newNode){
+        $tempNode = $this->next;
+        $this->next = $newNode;
+        $newNode->next = $tempNode;
+    }
+}
+class SinglyLinkedList8{
+    public $arr;
+    public $head;
+
+    public function __construct($arr){
+        $this->head = count($arr) > 0 ? new node8($arr[0]) : new node8(null);
+        $currentNode = $this->head;
+        for($i=1; $i<count($arr); $i++){
+            $currentNode->next = new node8($arr[$i]);
+            $currentNode = $currentNode->next;
+        }
+    }
+
+    public function at($index){
+        $iterator = $this->head;
+        for($i=0; $i<$index; $i++){
+            $iterator = $iterator->next;
+            if($iterator == null) return null;
+        }
+        return $iterator;
+    }
+
+    public function preAppend($newNode){
+        $newNode->next = $this->head;
+        $this->head = $newNode;
+    }
+
+    public function popFront(){
+        $this->head = $this->head->next;
+    }
+
+    public function delete($index){
+        if($index == 0) return $this->popFront();
+        $iterator = $this->head;
+        for($i=0; $i<$index-1; $i++){
+            if($iterator->next == null) return null;
+            $iterator = $iterator->next;
+        }
+        $iterator->next = $iterator->next->next;
+    }
+
+    public function printList(){
+        $iterator = $this->head;
+        $str = "";
+        while($iterator != null){
+            $str .= $iterator->data. " ";
+            $iterator = $iterator->next;
+        }
+        echo $str .PHP_EOL;
+    }
+}
+$numList = new SinglyLinkedList8([35,23,546,67,86,234,56,767,34,1,98,78,555]);
+
+$numList->printList();
+
+$numList->popFront();
+$numList->popFront();
+$numList->printList();
+
+$numList->delete(4);
+$numList->printList();
+
+$numList->delete(9);
+$numList->printList();
+?>
