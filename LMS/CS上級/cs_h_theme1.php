90d5a023827b203201363a34d354837ce2ee535d
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-11-29 21:37:59 +0900

    終了

--- LMS/CS上級/cs_h_theme1.php
+++ LMS/CS上級/cs_h_theme1.php
@@ -117,13 +117,17 @@ class SinglyLinkedListNode3{
     }
 }
 
-function deleteTail(?SinglyLinkedListNode3 $head):int{
+function deleteTail(?SinglyLinkedListNode3 $head){
     $current = $head;
-
+    $array = "";
     while($current->next !== null){
+        $array .= $current->data . "→";
         $current = $current->next;
+        if($current->next === null){
+            $array .= "END";
+        }
     }
-    return $current->data;
+    return $array;
 }
 
 function SinglyLinkedList3(array $values): ?SinglyLinkedListNode3{
@@ -141,27 +145,108 @@ function SinglyLinkedList3(array $values): ?SinglyLinkedListNode3{
     return $head;
 }
 
-function printList(array $values){
-    $iterator = $this->head;
-    $str = "";
-    while($iterator != null){
-        $str .= $iterator->data. " ";
-        $iterator = $iterator->next;
+
+echo "問題3".PHP_EOL;
+$array = SinglyLinkedList3([3,3,2,10,34,45,67,356]);
+echo deleteTail($array).PHP_EOL;
+
+$array = SinglyLinkedList3([8,7,21,3,2,7]);
+echo deleteTail($array).PHP_EOL;
+
+$array = SinglyLinkedList3([8,8,7,7,5]);
+echo deleteTail($array).PHP_EOL;
+
+$array = SinglyLinkedList3([8,6,3,5,7]);
+echo deleteTail($array).PHP_EOL;
+
+$array = SinglyLinkedList3([8,8,7,7,6,6,5,5,4,4]);
+echo deleteTail($array).PHP_EOL;
+
+$array = SinglyLinkedList3([2,5,10,25,50]);
+echo deleteTail($array).PHP_EOL;
+
+$array = SinglyLinkedList3([20,-5,34,45,67,356,34,687,31,-34,5]);
+echo deleteTail($array).PHP_EOL;
+
+?>
+
+
+
+
+<?php
+class SinglyLinkedListNode4{
+    public $data;
+    public ?SinglyLinkedListNode4 $next;
+
+    public function __construct($data){
+        $this->data = $data;
+        $this->next = null;
     }
-    echo $str .PHP_EOL;
 }
 
-echo "問題2".PHP_EOL;
-$head = SinglyLinkedList3([3,2,1,5,6,4]);
-echo deleteTail($head).PHP_EOL;
+function findingMinNUm(?SinglyLinkedListNode4 $head):int{
+    $minValue = $head->data;
+    $current = $head;
+    $minIndex = 0;
+    $currentIndex = 0;
 
-$head = SinglyLinkedList3([7,8,2,3,1,7,8,11,4,3,2]);
-echo deleteTail($head).PHP_EOL;
+    while($current !== null){
+        if($current->data < $minValue){
+            $minValue = $current->data;
+            $minIndex = $currentIndex;
+        }
+        if($current->data === $minValue){
+            $minValue = $current->data;
+            $minIndex = $currentIndex;
+        }
+        $current = $current->next;
+        $currentIndex ++;
+    }
+    return $minIndex;
+}
 
-$head = SinglyLinkedList3([34,35,64,34,10,2,14,5,353,23,35,63,23]);
-echo deleteTail($head).PHP_EOL;
+function SinglyLinkedList4(array $values): ?SinglyLinkedListNode4{
+    if(empty($values)){
+        return null;
+    }
 
-$head = SinglyLinkedList3([1]);
-echo deleteTail($head).PHP_EOL;
+    $head = new SinglyLinkedListNode4($values[0]);
+    $current = $head;
+
+    for($i=1; $i<count($values); $i++){
+        $current->next = new SinglyLinkedListNode4($values[$i]);
+        $current = $current->next;
+    }
+    return $head;
+}
+
+
+echo "問題4".PHP_EOL;
+$array = SinglyLinkedList4([1,2,3,4,-1,2]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([1,1,1]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([3,3,2,10,34,45,67,356]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([20,-5,34,45,67,356,34,687,31,-34,5]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([71,8,16,33]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([101,54,82002,93,1207,131,1800,99]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([580,781]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([2,4,52,108]);
+echo findingMinNum($array).PHP_EOL;
+
+$array = SinglyLinkedList4([61,73,27,3001]);
+echo findingMinNum($array).PHP_EOL;
 
 ?>
