41d8b19a3758331a2df95df9a309d27f2b10a57a
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-12-14 18:17:38 +0900

    終了

--- LMS/CS上級/cs_h_theme3.php
+++ LMS/CS上級/cs_h_theme3.php
@@ -22,34 +22,30 @@ function bstSearch(?BinaryTree $root, int $key): ?BinaryTree{
 
     if($key < $root->data){
         return bstSearch($root->left, $key);
-    } else {
+    }else{
         return bstSearch($root->right, $key);
     }
 }
 
-function bstSearchHelper($arr, $start, $end){
-    if($start > $end){
-        return null;
-    }
-
-    $mid = floor(($start + $end) / 2);
-
-    $left = bstSearchHelper($arr, $start, $mid-1);
-    $right = bstSearchHelper($arr, $mid+1, $end);
-
-    return new BinaryTree($arr[$mid], $left, $right);
-}
-
-function ToBinaryTree($nums){
+function toBinaryTree(array $nums): ?BinaryTree{
     if(count($nums) === 0){
         return null;
     }
 
-    $nums = array_filter($nums, fn($val)=>$val !== null);
-    sort($nums);
-    return bstSearchHelper($nums, 0, count($nums)-1);
-}
+    $n = count($nums);
+    $nodes = array_map(fn($val)=>$val === null ? null : new BinaryTree($val), $nums);
 
+    for($i=0; $i<$n; $i++){
+        if($nodes[$i] !== null){
+            $leftIndex = 2 * $i + 1;
+            $rightIndex = 2 * $i + 2;
+
+            $nodes[$i]->left = $leftIndex < $n ? $nodes[$leftIndex] : null;
+            $nodes[$i]->right = $rightIndex < $n ? $nodes[$rightIndex] : null;
+        }
+    }
+    return $nodes[0];
+}
 
 function treeToArray(?BinaryTree $root): array{
     if($root === null){
@@ -58,14 +54,15 @@ function treeToArray(?BinaryTree $root): array{
 
     $queue = [$root];
     $result = [];
-    while(!empty($queue)) {
+
+    while(!empty($queue)){
         $node = array_shift($queue);
 
         if($node !== null){
             $result[] = $node->data;
             $queue[] = $node->left;
             $queue[] = $node->right;
-        } else {
+        }else{
             $result[] = null;
         }
     }
@@ -73,17 +70,42 @@ function treeToArray(?BinaryTree $root): array{
     while(end($result) === null){
         array_pop($result);
     }
+
     return $result;
 }
 
-$tree = toBinaryTree([0, -10, 5, null, -3, null, 9]);
-$keyToFind = 5;
+$resultNode = bstSearch(toBinaryTree([0, -10, 5, null, -3, null, 9]), 5);
+print_r(treeToArray($resultNode));
 
-$resultNode = bstSearch($tree, $keyToFind);
+$resultNode = bstSearch(toBinaryTree([0, -10, 5, null, -3, null, 9]), 20);
+print_r(treeToArray($resultNode));
 
-if($resultNode !== null) {
-    $subtreeArray = treeToArray($resultNode);
-    print_r($subtreeArray);
-}else{
-    echo "Key $keyTiFind not found in the BST" .PHP_EOL;
+$resultNode = bstSearch(toBinaryTree([5,3,6,2,4,null,7]), 3);
+print_r(treeToArray($resultNode));
+
+$resultNode = bstSearch(toBinaryTree([5,3,6,2,4,null,7]), 5);
+print_r(treeToArray($resultNode));
+
+$resultNode = bstSearch(toBinaryTree([5,3,6,2,4,null,7]), 15);
+print_r(treeToArray($resultNode));
+
+
+?>
+
+
+<?php
+class BinaryTree2{
+    public $data;
+    public ?BinaryTree2 $left;
+    public ?BinaryTree2 $right;
+
+    public function __construct($data, $left=null, $right=null){
+        $this->data = $data;
+        $this->left = $left;
+        $this->right = $right;
+    }
+}
+
+function exists(?BinaryTree2 $root, int $key): bool{
+
 }
